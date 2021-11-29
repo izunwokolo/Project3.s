6b21c9fc4755c00f18e1b9903a3c7f4cc98cd233
izunwokolo <69478171+izunwokolo@users.noreply.github.com>
2021-11-29 13:39:42 -0500

    Update Project3.s

--- Project3.s
+++ Project3.s
@@ -1,9 +1,9 @@
 .data
 
-   data: .space 1001
-  answer: .asciiz"\n"
-  invalid: .asciiz "Invalid"
-  comma: .asciiz ","
+   input: .space 1001
+	output: .asciiz"\n"
+	invalid: .asciiz "incorrect"
+	comma: .asciiz ","
 
 .text
   main:
@@ -11,4 +11,16 @@
    la $a0, data #Getting User Input
    li $a1, 1001
    syscall
+   jal SubA
+
+count:
+		j print
+SubA:
+
+		sub $sp, $sp,4 #creates stack space
+		sw $a0, 0($sp) #puts input in stack
+		lw $t1, 0($sp) # stores the input into $t1
+		addi $sp,$sp,4 # moves the stack pointer up
+		move $t7, $t1 # stores beginning of input into $t7
+
 
