73b8551930acbf0ac0cc50aff96c33c25256e63a
izunwokolo <69478171+izunwokolo@users.noreply.github.com>
2021-11-30 16:42:42 -0500

    Update Project3.s

--- Project3.s
+++ Project3.s
@@ -75,4 +75,19 @@
 		addi $t1,$t1,1
 		beq $s0, 44, insub
 		j inval #jumps to the beginning of loop
+	insub:
+
+		addi $t2,$t2,1 #amount of substring
+		sub $sp, $sp,4# creating space in stack
+
+		sw $t0, 0($sp) #puts $t7 into the stack
+
+		move $t7,$t1  # store the pointer to the bit after the comma
+		lb $s0, ($t1) # loads the bit that $t1 is -> to
+		beq $s0, 0, count 
+		beq $s0, 10, count
+		beq $s0,44, inval
+		li $t4,0 #resets the amount of valid characters to 0
+		li $t3,0 #resets my space checker  to 0
+		j check1
 
